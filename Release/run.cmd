a26a853c4b65a72d97004f33f4833ca6815afad1
surak <holger@hfischer.org>
2016-01-22 11:06:14 +0100

    init

--- Release/run.cmd
+++
@@ -1,4 +0,0 @@
-REM /*das configfile ist optional. ist es nicht angegeben wird das jar-interne verwendet*/
-java -jar ../bootstrapper/target/bootstrapper-1.0-SNAPSHOT.jar --spring.config.location=application.properties 
-REM /*das commandline argument überschreibt hier den inhalt des propüertyfiles -> name=spring statt holger2*/
-REM java -jar ../Bootstrapper/target/Bootstrapper-1.0-SNAPSHOT.jar --spring.config.location=application.properties --name="Spring"
