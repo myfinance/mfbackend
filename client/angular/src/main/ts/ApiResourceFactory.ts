1ea372efa39afbf558a3406efa3b827d3fc0f510
HolgerFischer <holger@hfischer.org>
2016-06-21 10:46:18 +0200

    [MYF-56] change port for spring boot to avoid conflicts with wildfly

--- client/angular/src/main/ts/ApiResourceFactory.ts
+++ client/angular/src/main/ts/ApiResourceFactory.ts
@@ -5,7 +5,7 @@ hf.marketdataprovider.factories.factory('hf.marketdataprovider.apiResource', ['$
         ($resource:ng.resource.IResourceService):
             ng.resource.IResourceClass<ng.resource.IResource<any>> => {
             var apiRoot:ng.resource.IResourceClass<ng.resource.IResource<any>> =
-                $resource('http://localhost:8080/get/products');
+                $resource('http://localhost:8082/get/products');
 
             return apiRoot;
         }]);
