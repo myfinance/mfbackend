198c00905f2d75a1cab8c1fe16323baf41f91a94
HolgerFischer <holger@hfischer.org>
2016-08-26 17:46:45 +0200

    karaf custom dist next try

---
+++ app/service-api/src/main/java/de/hf/marketdataprovider/service/api/ServiceApi.java
@@ -0,0 +1,22 @@
+/** ----------------------------------------------------------------------------
+ *
+ * ---          DZ Bank FfM - Application Development                       ---
+ *              Copyright (c) 2014, ... All Rights Reserved
+ *
+ *
+ *  Project     : marketDataProvider
+ *
+ *  File        : ServiceApi.java
+ *
+ *  Author(s)   : xn01598
+ *
+ *  Created     : 26.08.2016
+ *
+ * ----------------------------------------------------------------------------
+ */
+
+package de.hf.marketdataprovider.service.api;
+
+public interface ServiceApi {
+    void testService();
+}
