ec99ea90fe1d51f5a569783cd5236d5a920b984f
HolgerFischer <holger@hfischer.org>
2017-01-20 13:06:04 +0100

    [MYF-119]  Jaas with ds instead of blueprint

--- bundles/security/src/main/java/de/hf/dac/security/auth/CustomJaasRealmService.java
+++ bundles/security/src/main/java/de/hf/dac/security/auth/CustomJaasRealmService.java
@@ -31,7 +31,7 @@ import java.util.Map;
 @Component(immediate = true)
 public class CustomJaasRealmService implements JaasRealm
 {
-    public static final String REALM_NAME = "customRealm";
+    public static final String REALM_NAME = "karaf";
 
     private AppConfigurationEntry[] configEntries;
 
@@ -40,10 +40,17 @@ public class CustomJaasRealmService implements JaasRealm
     {
         // create the configuration entry field using ProxyLoginModule class
 
+
+        configEntries = new AppConfigurationEntry[2];
+
+        addAuthentificationModule(bc);
+        addAuthorisationModule(bc);
+    }
+
+    private void addAuthentificationModule(BundleContext bc) {
         Map<String, Object> options = new HashMap<>();
-        configEntries = new AppConfigurationEntry[1];
         configEntries[0] = new AppConfigurationEntry(ProxyLoginModule.class.getName(),
-            AppConfigurationEntry.LoginModuleControlFlag.SUFFICIENT, options);
+            AppConfigurationEntry.LoginModuleControlFlag.REQUIRED, options);
 
         // actual LoginModule class name will be passed using the options object
 
@@ -59,7 +66,65 @@ public class CustomJaasRealmService implements JaasRealm
 
         // add extra options if needed; for example, karaf encryption
         // ....
+
+        options.put("java.naming.provider.url", "${auth.ldap.url}");
+        options.put("bindDN", "${auth.ldap.user}");
+        options.put("bindCredential", "${auth.ldap.credentials}");
+        options.put("baseCtxDN", "${auth.ldap.baseCtxDN}");
+        options.put("searchTimeLimit", "30000");
+        options.put("baseFilter", "(sAMAccountName={0})");
+        options.put("password-stacking", "useFirstPass");
+        options.put("storePass", "true");
+        options.put("useFirstPass", "false");
+        options.put("rolesCtxDN", "${auth.ldap.rolesCtxDN}");
+        options.put("roleFilter", "(sAMAccountName={0})");
+        options.put("roleAttributeID", "memberOf");
+        options.put("roleAttributeIsDN", "true");
+        options.put("roleNameAttributeID", "cn");
+        options.put("roleRecursion", "-1");
+        options.put("searchScope", "ONELEVEL_SCOPE");
+        options.put("allowEmptyPasswords", "false");
+        options.put("principalClass", "org.apache.karaf.jaas.boot.principal.UserPrincipal");
+        options.put("debug", "false");
     }
+
+    private void addAuthorisationModule(BundleContext bc) {
+        Map<String, Object> options = new HashMap<>();
+        configEntries[1] = new AppConfigurationEntry(ProxyLoginModule.class.getName(),
+            AppConfigurationEntry.LoginModuleControlFlag.REQUIRED, options);
+
+        // actual LoginModule class name will be passed using the options object
+
+        options.put(ProxyLoginModule.PROPERTY_MODULE, LdapLoginModule.class.getName());
+
+        // put bundle id of the LoginModule and bundlecontext of it
+        // (in this case, it is the same bundle)
+        // This is a neat trick to adapt to OSGI classloader
+
+        long bundleId = bc.getBundle().getBundleId();
+        options.put(ProxyLoginModule.PROPERTY_BUNDLE, String.valueOf(bundleId));
+        options.put(BundleContext.class.getName(), bc);
+
+        // add extra options if needed; for example, karaf encryption
+        // ....
+
+        options.put("java.naming.provider.url", "${role.ldap.url}");
+        options.put("bindDN", "${role.ldap.user}");
+        options.put("bindCredential", "${role.ldap.credentials}");
+        options.put("baseCtxDN", "${role.ldap.baseCtxDN}");
+        options.put("baseFilter", "(dzuid={0})");
+        options.put("rolesCtxDN", "${role.ldap.rolesCtxDN}");
+        options.put("roleFilter", "(uniqueMember={1})");
+        options.put("roleAttributeID", "cn");
+        options.put("permissionAttributeID", "dzpermissions");
+        options.put("searchScope", "ONELEVEL_SCOPE");
+        options.put("allowEmptyPasswords", "false");
+        options.put("useFirstPass", "true");
+        options.put("permissionRoleMapping", "${role.mappings}");
+        options.put("userAttributes", "${role.userAttributes}");
+        options.put("debug", "false");
+    }
+
 
     @Override
     public AppConfigurationEntry[] getEntries()
@@ -78,6 +143,6 @@ public class CustomJaasRealmService implements JaasRealm
     @Override
     public int getRank()
     {
-        return 0;
+        return 2;
     }
 }
