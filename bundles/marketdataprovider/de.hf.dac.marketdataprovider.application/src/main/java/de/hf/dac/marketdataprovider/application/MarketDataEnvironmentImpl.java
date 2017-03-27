fb8ebe0cee3fd47ba99a61a7f2eca4a87f51495d
HolgerFischer <holger@hfischer.org>
2017-03-27 14:21:51 +0200

    [MYF-95]add camel

--- bundles/marketdataprovider/de.hf.dac.marketdataprovider.application/src/main/java/de/hf/dac/marketdataprovider/application/MarketDataEnvironmentImpl.java
+++ bundles/marketdataprovider/de.hf.dac.marketdataprovider.application/src/main/java/de/hf/dac/marketdataprovider/application/MarketDataEnvironmentImpl.java
@@ -20,13 +20,19 @@ package de.hf.dac.marketdataprovider.application;
 import de.hf.dac.api.io.routes.job.JobDispatcher;
 import de.hf.dac.api.io.routes.job.JobParameter;
 import de.hf.dac.api.security.RootSecurityProvider;
+import de.hf.dac.api.security.SecurityServiceBuilder;
 import de.hf.dac.marketdataprovider.api.application.MarketDataEnvironment;
 import de.hf.dac.marketdataprovider.api.application.OpLevel;
+import de.hf.dac.marketdataprovider.api.application.OpType;
 import de.hf.dac.marketdataprovider.api.service.ProductService;
 import de.hf.dac.marketdataprovider.api.service.InstrumentService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,15 +40,37 @@ public class MarketDataEnvironmentImpl implements MarketDataEnvironment{
 
     final private ProductService productService;
     final private InstrumentService instrumentService;
-    final private RootSecurityProvider rootSecurityProvider;
     final private String environment;
 
+    private RootSecurityProvider rootSecurityProvider;
+
+    //todo gemeinsamkeiten mit runnerRoot in abstracte klasse auslagern
+    /**
+     * Not all Requests have an Environment e.G. listRunner (there are jobs for multiple environments listed).
+     * so I have to configure a environment to secure these operations as well
+     */
+    @ObjectClassDefinition(name = "Runner Security Source Configuration")
+    public @interface RunnerRootSecurity {
+        String sourceEnvironmentForSecurityDB() default "dev";
+    }
+
+    @Reference
+    SecurityServiceBuilder<OpType, OpLevel> securityServiceBuilder;
+
     @Inject
-    public MarketDataEnvironmentImpl(RootSecurityProvider rootSecurityProvider, ProductService productService, InstrumentService instrumentService, @Named("envID") String environment){
+    public MarketDataEnvironmentImpl(ProductService productService, InstrumentService instrumentService, @Named("envID") String environment){
         this.productService=productService;
         this.instrumentService=instrumentService;
-        this.rootSecurityProvider=rootSecurityProvider;
         this.environment=environment;
+    }
+
+    @Activate
+    private void activate(RunnerRootImpl.RunnerRootSecurity cacheRootSecurity) {
+        try {
+            rootSecurityProvider = securityServiceBuilder.build(cacheRootSecurity.sourceEnvironmentForSecurityDB());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
