0646f86122f6e1e44bc220e876b82dd7646098fb
HolgerFischer <holger@hfischer.org>
2017-10-20 16:16:26 +0200

    [MYF-161]local deployment for integrationtests

---
+++ distributions/deploy-myfinance/deploy.bat
@@ -0,0 +1,98 @@
+echo "start deploy"
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+rem ----------------------------------------------------------------------
+rem Setup & Initialisierung
+rem ----------------------------------------------------------------------
+set appname=%~n0
+set verbose=0
+set mydir=%~dp0
+set targetdir=c:\temp
+
+rem ----------------------------------------------------------------------
+rem Argument Parsen
+rem ----------------------------------------------------------------------
+
+:parse_args
+	if [%~1] == [] (
+		goto done_args
+	)
+	set arg=%~1
+	if not [%arg:~0,1%] == [-] (
+		goto done_args
+	)
+	if [%arg%] == [-v] (
+		set verbose=1
+		call:cout Set Verbose on
+		goto next_arg
+	)
+	if [%arg%] == [-t] (
+		set targetdir=%~2
+		goto next_arg
+	)
+	goto usage
+:next_arg
+	shift
+	goto parse_args
+:done_args
+	set arg=
+
+rem ----------------------------------------------------------------------
+rem deploy
+rem ----------------------------------------------------------------------
+call:cout %targetdir%
+if exist %targetdir%\karaf (
+    rmdir %targetdir%\karaf /s /q
+)
+call:cout %mydir%
+mkdir %targetdir%\karaf
+copy %mydir%\..\myfinance-full-packaging\target\run-job-bin.zip %targetdir%\karaf
+copy /y %mydir%\..\karaf-full\integrationtest.res %targetdir%\dac.res
+cd %targetdir%\karaf
+unzip %targetdir%\karaf\run-job-bin.zip
+Start "" "%targetdir%\karaf\karaf\bin\karaf_local.bat"
+
+:end_with_msg
+	call:cout Done.
+	REM /*FALLTHROUGH*/
+
+goto end
+
+rem ----------------------------------------------------------------------
+rem Funktionen & Sprungziele
+rem ----------------------------------------------------------------------
+
+:cout
+	echo %appname%: %*
+	goto:EOF
+
+:cerr
+	echo %appname%: ERROR: %*
+	goto:EOF
+
+:dbgmsg
+	if %verbose% neq 0 (
+		echo %appname%: %*
+	)
+	goto:EOF
+:usage
+	echo.
+	echo Deploy myfinance for integrationtests
+	echo.
+	echo call: %appname%.cmd [OPTIONEN] [
+	echo.
+	echo Optionen:
+	echo.    -v           print additional infos
+	echo.    -t [target] change the default target directory
+	echo.
+	exit /b 1
+
+:failure
+	echo %appname%: error see above
+	echo.
+	exit /b 1
+
+:end
+endlocal
+
