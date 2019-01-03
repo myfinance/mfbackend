7389c95ea6818459acad667ba343ffd212caeac4
HolgerFischer <holger.fischer@dzbank.de>
2019-01-03 16:11:29 +0100

    [MYF-250] update dac schema for message table

---
+++ bundles/api/de.hf.dac.api.io/src/main/java/de/hf/dac/api/io/domain/Serverity.java
@@ -0,0 +1,19 @@
+package de.hf.dac.api.io.domain;
+
+/**
+ * ----------------------------------------------------------------------------
+ * ---          HF - Application Development                       ---
+ * Copyright (c) 2014, ... All Rights Reserved
+ * Project     : dac
+ * File        : Serverity.java
+ * Author(s)   : hf
+ * Created     : 03.01.2019
+ * ----------------------------------------------------------------------------
+ */
+
+public enum Serverity {
+    INFO,
+    WARN,
+    ERROR,
+    FATAL
+}
