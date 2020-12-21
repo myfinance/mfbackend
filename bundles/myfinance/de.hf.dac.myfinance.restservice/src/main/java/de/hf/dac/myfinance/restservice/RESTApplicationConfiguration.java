2f93fddbf261411ad96bc368931e494abc4ee79f
holgerfischer <holger@hfischer.org>
2020-12-21 08:54:46 +0000

    cors allow all
    Signed-off-by: holgerfischer <holger@hfischer.org>

--- bundles/myfinance/de.hf.dac.myfinance.restservice/src/main/java/de/hf/dac/myfinance/restservice/RESTApplicationConfiguration.java
+++ bundles/myfinance/de.hf.dac.myfinance.restservice/src/main/java/de/hf/dac/myfinance/restservice/RESTApplicationConfiguration.java
@@ -42,7 +42,8 @@ public @interface RESTApplicationConfiguration {
     String corsAllowedHeaders() default "";
 
     // If empty allow any origin - if filled with comma separated origins allow only those
-    String corsAllowedOrigins() default "http://localhost:4300,http://localhost:4200,http://localhost:8080,http://prod.mffe.mylocaldomain,http://dev.mffe.mylocaldomain,https://babcom.myds.me,https://4200-c4cf8608-3f2c-4d68-9466-bc24b0c69f4a.ws-eu03.gitpod.io,*";
+    String corsAllowedOrigins() default "";
+//String corsAllowedOrigins() default "http://localhost:4300,http://localhost:4200,http://localhost:8080,http://prod.mffe.mylocaldomain,http://dev.mffe.mylocaldomain,https://babcom.myds.me,https://4200-c4cf8608-3f2c-4d68-9466-bc24b0c69f4a.ws-eu03.gitpod.io,*";
 
     // If empty allow any methods - if filled with comma separated methods allow only those
     String corsAllowedMethods() default "GET,POST,DELETE,PUT,OPTIONS";
